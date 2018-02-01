54c610ad622d921fe6b209cc2541a7071590b7f0
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2018-02-01 11:37:54 +0200

    Implement DbContextFactory

---
+++ DesignTimeDbContextFactory.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace todolist.DbContext
+{
+  public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+  {
+    public AppDbContext CreateDbContext(string[] args)
+    {
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+      var builder = new DbContextOptionsBuilder<AppDbContext>();
+
+      var connStr = configuration.GetConnectionString("DefaultConnection");
+      builder.UseSqlServer(connStr);
+      return new AppDbContext(builder.Options);
+    }
+  }
+}
