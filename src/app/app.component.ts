e97bc1f056592f68f1bb467b70de01ebe93399d5
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2018-02-01 12:52:47 +0200

    Typescript part of component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'app/services/task.service';
+import { Task, AddTask } from 'app/models/task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  tasks: Task[] = [];
+  task: AddTask = new AddTask();
   title = 'app';
+  createVisible = false;
+  constructor(private _taskService: TaskService) {
+
+  }
+
+  ngOnInit() {
+    this._taskService.getTasks()
+      .subscribe((response) => {
+        this.tasks = response;
+        console.log(response);
+      });
+  }
+
+  Add(model: Task) {
+    this._taskService.Add(model)
+      .subscribe((response) => {
+        console.log(response);
+        this.tasks.push(response);
+        this.createVisible = !this.createVisible;
+      });
+  }
+
+  Update(model: Task) {
+    this._taskService.Update(model.id, model)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  Delete(model: Task) {
+    this._taskService.Delete(model.id)
+      .subscribe((response) => {
+        console.log(response);
+        this.tasks.splice(this.tasks.indexOf(model), 1);
+      });
+  } 
+
 }
